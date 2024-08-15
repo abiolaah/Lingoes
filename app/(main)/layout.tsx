d48c4ba873d5d2118a6211ffb801a3c4ab5cc680
victoriaajuwon <oluwapelumi.ajuwon@gmail.com>
2024-08-15 12:09:36 -0600

    Refactor for mobile sytling

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,3 +1,4 @@
+import { MobileHeader } from "@/components/mobile-header";
 import { Sidebar } from "@/components/sidebar";
 
 type Props = {
@@ -6,6 +7,7 @@ type Props = {
 const MainLayout = ({ children }: Props) => {
   return (
     <>
+      <MobileHeader />
       <Sidebar className="hidden lg:flex" />
       <main className="lg:pl-[256px] h-full pt-[50px] lg:pt-0">
         <div className="bg-red-500 h-full">{children}</div>
