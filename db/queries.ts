64cf28dbf303b11a4627b821a0efda49e79be948
victoriaajuwon <oluwapelumi.ajuwon@gmail.com>
2024-08-16 23:09:44 -0600

    Refactor for get c ourse by id method

--- db/queries.ts
+++ db/queries.ts
@@ -2,7 +2,7 @@ import { cache } from "react";
 import db from "./drizzle";
 import { auth } from "@clerk/nextjs/server";
 import { eq } from "drizzle-orm";
-import { userProgress } from "@/db/schema";
+import { courses, userProgress } from "@/db/schema";
 
 export const getUserProgress = cache(async () => {
   const { userId } = await auth();
@@ -26,3 +26,15 @@ export const getCourses = cache(async () => {
 
   return data;
 });
+
+export const getCourseById = cache(async (courseId: number) => {
+  const data = await db.query.courses.findFirst({
+    where: eq(courses.id, courseId),
+    //TODO: Populate units and lessons
+  });
+
+  return data;
+});
+
+//When committing: You made the following changes:
+//Add new method to get  course by id.
