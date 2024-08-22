6457b72e8f6032366ea8f2cecab19db871ce853c
victoriaajuwon <oluwapelumi.ajuwon@gmail.com>
2024-08-22 17:09:34 -0600

    Add new files for lesson page

---
+++ app/lesson/quiz.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import { useState } from "react";
+
+import { challengeOptions, challenges } from "@/db/schema";
+import { Header } from "./header";
+
+type Props = {
+  initialLessonId: number;
+  initialLessonChallenge: (typeof challenges.$inferSelect & {
+    completed: boolean;
+    challengeOptions: (typeof challengeOptions.$inferSelect)[];
+  })[];
+  initialHearts: number;
+  initialPercentage: number;
+  userSubscription: any; //TODO: Replace with subscription DB type
+};
+
+export const Quiz = ({
+  initialLessonId,
+  initialLessonChallenge,
+  initialHearts,
+  initialPercentage,
+  userSubscription,
+}: Props) => {
+  const [hearts, setHearts] = useState(initialHearts);
+  const [percentage, setPercentage] = useState(initialPercentage);
+  return (
+    <>
+      <Header
+        hearts={hearts}
+        percentage={percentage}
+        hasActiveSubscription={!!userSubscription?.isActive}
+      />
+    </>
+  );
+};
