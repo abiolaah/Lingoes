3db645758616fa164715d60312dfd2cebdfe6a0a
victoriaajuwon <oluwapelumi.ajuwon@gmail.com>
2024-09-05 17:59:50 -0600

    Refactor file to include subscription logic

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -10,6 +10,7 @@ import {
   getLessonPercentage,
   getUnits,
   getUserProgress,
+  getUserSubscription,
 } from "@/db/queries";
 import { Unit } from "./unit";
 import { lessons, units as unitsSchema } from "@/db/schema";
@@ -19,14 +20,21 @@ const LearnPage = async () => {
   const courseProgressPromise = getCourseProgress();
   const lessonPercentagePromise = getLessonPercentage();
   const unitsPromise = getUnits();
+  const userSubscriptionPromise = getUserSubscription();
 
-  const [userProgress, units, courseProgress, lessonPercentage] =
-    await Promise.all([
-      userProgressPromise,
-      unitsPromise,
-      courseProgressPromise,
-      lessonPercentagePromise,
-    ]);
+  const [
+    userProgress,
+    units,
+    courseProgress,
+    lessonPercentage,
+    userSubscription,
+  ] = await Promise.all([
+    userProgressPromise,
+    unitsPromise,
+    courseProgressPromise,
+    lessonPercentagePromise,
+    userSubscriptionPromise,
+  ]);
 
   if (!userProgress || !userProgress.activeCourse) {
     redirect("/courses");
@@ -43,7 +51,7 @@ const LearnPage = async () => {
           activeCourse={userProgress.activeCourse}
           hearts={userProgress.hearts}
           points={userProgress.points}
-          hasActiveSubscription={false}
+          hasActiveSubscription={!!userSubscription?.isActive}
         />
       </StickyWrapper>
       <FeedWrapper>
