0906980f5cf96c4e0ecbaed57188060ce584b828
victoriaajuwon <oluwapelumi.ajuwon@gmail.com>
2024-10-08 15:39:02 -0600

    Refactor file to include component for subscribed courses and their logic

--- components/user-progress.tsx
+++ components/user-progress.tsx
@@ -1,34 +1,68 @@
+"use client";
+
 import Link from "next/link";
 import Image from "next/image";
 
-import { InfinityIcon, SquarePlus } from "lucide-react";
+import { InfinityIcon } from "lucide-react";
 
 import { Button } from "./ui/button";
 import { courses } from "@/db/schema";
-import {
-  DropdownMenu,
-  DropdownMenuTrigger,
-  DropdownMenuContent,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
-  DropdownMenuCheckboxItem,
-  DropdownMenuItem,
-} from "./ui/dropdown-menu";
+import { redirect, useRouter } from "next/navigation";
+import { toast } from "sonner";
+import { useTransition } from "react";
+import { upsertUserProgress, unSubscribeCourse } from "@/actions/user-progress";
+import { SubscribedCourses } from "./subscribed-courses";
 
 type Props = {
   activeCourse: typeof courses.$inferSelect;
   hearts: number;
   points: number;
+  subscribedCourses: any[];
   hasActiveSubscription: boolean;
 };
 
-// type Checked = DropdownMenuCheckboxItemProps["checked"];
 export const UserProgress = ({
   activeCourse,
   hearts,
   points,
+  subscribedCourses,
   hasActiveSubscription,
 }: Props) => {
+  const router = useRouter();
+  const [pending, startTransition] = useTransition();
+
+  const onClick = (courseId: number) => {
+    if (pending) return; // if the state is pending, just break out of the function
+
+    // if the user clicks on a course that is active for them, just break out of the function and redirect them to the learn page
+    if (courseId === activeCourse.id) {
+      return router.push("/learn");
+    }
+
+    console.log("COURSE ID from onClick", courseId);
+
+    startTransition(() => {
+      upsertUserProgress(courseId)
+        .then(() => {
+          // Optionally handle the success state
+          router.push("/learn"); // Redirect to learn page after progress update
+        })
+        .catch(() => {
+          toast.error("Something went wrong.");
+        });
+    });
+  };
+
+  const onUnSubscribe = async (courseId: number) => {
+    try {
+      console.log("COURSE ID from unsubscribed", courseId);
+      await unSubscribeCourse(courseId); // Call the handleUnSubscribe function
+      toast.success("You have unsubscribed from the course.");
+      router.refresh(); // Reload the current page instead of redirecting
+    } catch (error) {
+      toast.error("Error unsubscribing from the course.");
+    }
+  };
   return (
     <div className="flex items-center justify-between gap-x-2 w-full">
       <Link href="/courses" className="flex lg:hidden">
@@ -43,61 +77,12 @@ export const UserProgress = ({
         </Button>
       </Link>
 
-      <DropdownMenu>
-        <DropdownMenuTrigger asChild className="hidden lg:flex">
-          <Button variant="ghost">
-            <Image
-              src={activeCourse.imageSrc}
-              alt={activeCourse.title}
-              className="rounded-md border"
-              width={32}
-              height={32}
-            />
-          </Button>
-        </DropdownMenuTrigger>
-        <DropdownMenuContent className="w-56">
-          <DropdownMenuLabel className="text-lg text-neutral-400 font-bold text-center">
-            My Courses
-          </DropdownMenuLabel>
-          <DropdownMenuSeparator className="bg-neutral-500 h-1 rounded" />
-          <DropdownMenuItem className="flex gap-2 items-center justify-normal text-xl font-semibold mb-2">
-            <Image
-              src="/flags/l/fr.svg"
-              alt="French"
-              width={30}
-              height={30}
-              className="rounded"
-            />
-            French
-          </DropdownMenuItem>
-          <DropdownMenuItem className="flex gap-2 items-center justify-normal text-xl font-semibold">
-            <Image
-              src="/flags/l/es.svg"
-              alt="Spanish"
-              width={30}
-              height={30}
-              className="rounded"
-            />
-            Spanish
-          </DropdownMenuItem>
-          <DropdownMenuSeparator className="bg-neutral-500 h-1 rounded" />
-          <DropdownMenuItem>
-            <Link href="/courses" className="">
-              <Button
-                variant="ghost"
-                className="flex items-center justify-normal gap-2 text-sm"
-              >
-                <SquarePlus
-                  color="#b2b3b3"
-                  className="justify-items-start"
-                  size={35}
-                />
-                Add New Course
-              </Button>
-            </Link>
-          </DropdownMenuItem>
-        </DropdownMenuContent>
-      </DropdownMenu>
+      <SubscribedCourses
+        activeCourse={activeCourse}
+        subscribedCourses={subscribedCourses}
+        handleUnSubscribe={onUnSubscribe}
+        onClick={onClick}
+      />
 
       <Link href="/shop">
         <Button variant="ghost" className="text-orange-500">
